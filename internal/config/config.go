e353ff8feeafe27a2c46a6a3f38fe47f4b8599a3
TFMV <mcgeehan@gmail.com>
2026-04-02 18:30:39 -0500

    Add DuckStream v2

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,21 +3,27 @@ package config
 import "time"
 
 type Config struct {
-	DuckDBPath   string
-	QUICAddr     string
-	IngestAddr   string
-	BatchSize    int
-	BatchTimeout time.Duration
-	PollInterval time.Duration
+	DuckDBPath          string
+	QUICAddr            string
+	IngestAddr          string
+	BatchSize           int
+	BatchTimeout        time.Duration
+	PollInterval        time.Duration
+	MaxClients          int
+	MaxStreamsPerClient int
+	MaxQueries          int
 }
 
 func Default() *Config {
 	return &Config{
-		DuckDBPath:   "duckstream.db",
-		QUICAddr:     "localhost:4242",
-		IngestAddr:   "localhost:8080",
-		BatchSize:    100,
-		BatchTimeout: time.Second,
-		PollInterval: 100 * time.Millisecond,
+		DuckDBPath:          "duckstream.db",
+		QUICAddr:            "localhost:4242",
+		IngestAddr:          "localhost:8080",
+		BatchSize:           100,
+		BatchTimeout:        time.Second,
+		PollInterval:        100 * time.Millisecond,
+		MaxClients:          100,
+		MaxStreamsPerClient: 10,
+		MaxQueries:          50,
 	}
 }
